0137c721aa0b13d476c6b8bc738c80e6387f934f
yinccc <349064816@qq.com>
2019-03-24 22:04:24 +0800

    20190324

---
+++ 102-20190324-BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,27 @@
+#Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        if not root:
+            return []
+        ans, level = [], [root]
+        while level:
+            ans.append([node.val for node in level])
+            temp = []
+            for node in level:
+                temp.extend([node.left, node.right])
+            level = [leaf for leaf in temp if leaf]
+        return ans
+
+a=TreeNode(0)
+b=TreeNode(1)
+c=TreeNode(2)
+a.left=b
+a.right=c
+S=Solution()
+print(S.levelOrder(a))
